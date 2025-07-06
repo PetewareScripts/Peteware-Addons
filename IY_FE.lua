efcdc9ae8722a2baf7030a44e375de094d64bcfb
Peteware <petewarescripts@gmail.com>
2025-07-06 20:48:45 +0100

    Update IY_FE.lua

--- IY_FE.lua
+++ IY_FE.lua
@@ -1,15 +1,10 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 
 local plugin = [[
-return game:HttpGet("https://raw.githubusercontent.com/PetewareScripts/Peteware-Addons/refs/heads/main/main.lua")
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/PetewareScripts/Peteware-Addons/refs/heads/main/main.lua"))()
 ]]
 
 local file = "PetewareAddons.iy"
 pcall(function() writefile(file, plugin) end)
-
-if not (typeof(addPlugin) == "function" and typeof(LoadPlugin) == "function") then
-    repeat task.wait()
-    until typeof(addPlugin) == "function" and typeof(LoadPlugin) == "function"
-end
 
 addPlugin(file)
