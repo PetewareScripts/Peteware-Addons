6b73d9aee2c15920605ad31d7d455c35016bba73
Peteware <petewarescripts@gmail.com>
2025-07-07 08:01:08 +0100

    Update IY_FE.lua

--- IY_FE.lua
+++ IY_FE.lua
@@ -4,7 +4,13 @@ local plugin = [[
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/PetewareScripts/Peteware-Addons/refs/heads/main/main.lua"))()
 ]]
 
-local file = "PetewareAddons.iy"
+local mainFolder = "Peteware"
+
+if not isfolder(mainFolder) then
+    makefolder(mainFolder)
+end
+
+local file = mainFolder .. "/PetewareAddons.iy"
 pcall(function() writefile(file, plugin) end)
 
 addPlugin(file)
